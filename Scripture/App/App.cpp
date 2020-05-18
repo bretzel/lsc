38a1fdb09a1c5599f56ee95b381079497bd56263
bretzel <lussier.serge@gmail.com>
2020-05-18 11:24:01 -0400

    sync

--- Scripture/App/App.cpp
+++ Scripture/App/App.cpp
@@ -57,7 +57,7 @@ Return App::Type()
 }
 Return App::Tokens()
 {
-    TokenData Token_ = TokenData::Scan("sin(4/2a);");
+    TokenData Token_ = TokenData::Scan("sin+4/2a;"); // => sin(+4) / 2a;
     
     std::cout <<
         __PRETTY_FUNCTION__ << ":\n" <<
