23054b5b9697ebe07cbd94aefa53f88658972482
Serge Lussier <lussier.serge@gmail.com>
2020-10-29 12:01:22 -0400

    Adding TerminalAppApi sub project lib : VCell.

---
+++ TerminalAppApi/Src/Kernel/VCell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lsc on 20-10-29.
+//
+
+#include "Lsc/TerminalAppApi/Kernel/VCell.h"
---
+++ TerminalAppApi/include/Lsc/TerminalAppApi/Kernel/VCell.h
@@ -0,0 +1,16 @@
+//
+// Created by lsc on 20-10-29.
+//
+
+//#ifndef LSC_VCELL_H
+//#define LSC_VCELL_H
+#pragma once
+
+class VCell
+{
+
+};
+
+
+
+//#endif //LSC_VCELL_H
